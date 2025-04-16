35ec1a10f532a309f1427c07638a7ad0976c05f3
Chiara <chiara-zuc@libero.it>
2025-04-16 18:12:47 +0200

    filters fully working

--- src/app/ui/filters/filters.component.ts
+++ src/app/ui/filters/filters.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Quote } from '@entity/Quote.class';
 import { Filters } from '@interfaces/filters.interface';
+import { QuotesService } from '@services/quotes.service';
 
 type FiltersType = 'fav' | 'author' | 'typed' | 'category';
 
@@ -16,19 +17,32 @@ type FiltersType = 'fav' | 'author' | 'typed' | 'category';
 // filter by typed string
 // filter by author
 export class FiltersComponent implements OnInit {
-  @Input() originalList: Quote[];
-
-  public filteredList: Quote[];
+  private _quotesService = inject(QuotesService);
+  private copyList: Quote[];
+  
   public filters: Filters;
   public showFilters = false;
 
+  public selectedAuthors: string[] = [];
+  public selectedCategories: string[] = [];
+  
+  public openedAccordion: string[] = [];
+
   ngOnInit(): void {
-    this.filters = this.createFiltersFrom(this.originalList);
+    this.copyList = structuredClone(this._quotesService.quotes());
+    this.filters = this.createFiltersFrom(this.copyList);
   }
 
   private canAdd(item: string, itemsList: string[]) {
-    const findAthour= itemsList.findIndex(it => it.includes(item));
-    return findAthour === -1;
+    const indexFinded = itemsList.findIndex(it => it.includes(item));
+    return indexFinded === -1;
+  }
+
+  private deleteItem(item: string, itemsList: string[]) {
+    const indexFinded = itemsList.findIndex(it => it.includes(item));
+    if(indexFinded !== -1) {
+      itemsList.splice(indexFinded, 1);
+    }
   }
 
   private createFiltersFrom(list: Quote[]): Filters {
@@ -53,11 +67,64 @@ export class FiltersComponent implements OnInit {
     }
   }
 
-  public filterBy(type: FiltersType) {
+  public manageSelection(itemToAdd: string, type: 'author' | 'category') {
+    switch(type) {
+      case 'author':
+        if(this.canAdd(itemToAdd, this.selectedAuthors)) {
+          this.selectedAuthors.push(itemToAdd);
+        } else this.deleteItem(itemToAdd, this.selectedAuthors);
+        this.filterBy('author');
+      break;
+      case 'category':
+        if(this.canAdd(itemToAdd, this.selectedCategories)) {
+          this.selectedCategories.push(itemToAdd);
+        } else this.deleteItem(itemToAdd, this.selectedCategories);
+        this.filterBy('category');
+      break;
+    }
+  }
 
+  public filterBy(type: FiltersType) {
+    let filteredList: Quote[] = [];
+    switch(type) {
+      case 'fav':
+        filteredList = this.filters.favorites ? this.copyList.filter(copyQuote => copyQuote.isFavorite) : this.copyList;
+      break;
+      case 'author':
+        filteredList = this.selectedAuthors.length > 0 ? this.copyList.filter(copyQuote => this.selectedAuthors.includes(copyQuote.author)) : this.copyList;
+      break;
+      case 'typed':
+        const normalizedTyped = this.filters.typed.toLowerCase().replace(' ', '-');
+        filteredList = this.copyList.filter(copyQuote =>
+          copyQuote.description.toLowerCase().replace(' ', '-').includes(normalizedTyped) ||
+          copyQuote.author_slug.includes(normalizedTyped)
+        );
+      break;
+      case 'category':
+        filteredList = this.selectedCategories.length > 0 ? this.copyList.filter(
+          copyQuote => copyQuote.categories.some(cat => this.selectedCategories.includes(cat))
+        ) : this.copyList;
+      break;
+    }
+    this._quotesService.quotes.set(filteredList);
   }
 
   public openFilters() {
     this.showFilters = !this.showFilters;
+    this.openedAccordion = [];
+  }
+
+  public manageAccordion(key: 'author' | 'category') {
+    if(this.openedAccordion.length === 0) {
+      this.openedAccordion.push(key);
+      return;
+    }
+
+    const findedKey = this.openedAccordion.findIndex(acc => acc.includes(key));
+    this.openedAccordion = [];
+    if(findedKey === -1) {
+      this.openedAccordion.push(key);
+    }
+
   }
 }
