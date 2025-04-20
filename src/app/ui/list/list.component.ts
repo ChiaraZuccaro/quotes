9f4897ccc25cbdb710ac4580a48390b3fdcc9900
Chiara <chiara-zuc@libero.it>
2025-04-21 00:34:50 +0200

    filters now fix on scroll

--- src/app/ui/list/list.component.ts
+++ src/app/ui/list/list.component.ts
@@ -5,6 +5,8 @@ import { QuotesService } from '@services/quotes.service';
 import { FiltersComponent } from '../filters/filters.component';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ViewportScroller } from '@angular/common';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'list',
@@ -14,12 +16,17 @@ import { ViewportScroller } from '@angular/common';
 })
 export class ListComponent implements OnInit, OnDestroy {
   private _viewportScroller = inject(ViewportScroller);
+  private _breakpointObserver = inject(BreakpointObserver);
   private _route = inject(ActivatedRoute);
   public quotesService = inject(QuotesService);
 
+  private $destroy = new Subject();
+
   public list = input.required<Quote[]>();
 
-  public isFixVisible = signal(false);
+  public fixGoTop = signal(false);
+  public fixFilters = signal(false);
+  public isMobile = signal(false);
   public isExplorePage = false;
   public disableCard = computed(() => this.quotesService.userQuotes().some(qt => qt.isEditMode));
   public resortList = computed(() => {
@@ -29,16 +36,24 @@ export class ListComponent implements OnInit, OnDestroy {
 
   private checkScroll = (): void => {
     const [x, y] = this._viewportScroller.getScrollPosition();
-    this.isFixVisible.set(y > 200);
+    this.fixGoTop.set(y > 200);
+    this.fixFilters.set(y > 900);
   };
 
   ngOnInit(): void {
     this._route.url.subscribe(
     subUrl => this.isExplorePage = subUrl.length > 0 && subUrl[0].path === 'explore');
+
+    this._breakpointObserver.observe([Breakpoints.Handset]).pipe(
+      map(res => res.matches || window.innerWidth < 600), takeUntil(this.$destroy)
+    ).subscribe(isMobile => this.isMobile.set(isMobile));
+
     window.addEventListener('scroll', this.checkScroll);
   }
 
   ngOnDestroy(): void {
+    this.$destroy.next(true);
+    this.$destroy.complete();
     window.removeEventListener('scroll', this.checkScroll);
   }
 
