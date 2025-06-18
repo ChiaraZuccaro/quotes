80090114cf192d0e256774c1af26380149186a09
Chiara <chiara-zuc@libero.it>
2025-06-18 17:42:36 +0200

    pagination

--- src/app/ui/pagination/pagination.component.ts
+++ src/app/ui/pagination/pagination.component.ts
@@ -11,6 +11,11 @@ import { QuotesService } from '@services/quotes.service';
 export class PaginationComponent implements OnInit {
   private _quoteService = inject(QuotesService);
 
+  private startX = 0;
+  private startY = 0;
+  private scrollLeft = 0;
+  private scrollTop = 0;
+
   public dataPage = input.required<PaginationList>();
   public allPages: number[] = [];
 
@@ -26,4 +31,27 @@ export class PaginationComponent implements OnInit {
     this._quoteService.currentPage.set(newPg);
   }
 
+  public initDrag(event: MouseEvent) {
+    console.log('init drag -->', event);
+    const target = event.target as HTMLElement;
+    this.startX = event.pageX - target.offsetLeft;
+    this.startY = event.pageY - target.offsetTop;
+    this.scrollLeft = target.scrollLeft;
+    this.scrollTop = target.scrollTop;
+  }
+
+  public dragging(event: MouseEvent) {
+    console.log('dragging --> ', event);
+    event.preventDefault();
+    console.log(this.startX, this.startY);
+    
+    const moveX = event.pageX - this.startX;
+    const moveY = event.pageY - this.startY;
+    (event.currentTarget as HTMLElement).scrollLeft = this.scrollLeft - moveX;
+    (event.currentTarget as HTMLElement).scrollTop = this.scrollTop - moveY;
+  }
+
+  public endDrag() {
+
+  }
 }
